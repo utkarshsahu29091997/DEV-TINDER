c1e74f4e8137e4364bb8aa57e60f4da008a1cbf8
Utkarsh Sahu <utkarshsahugithub101@gmail.com>
2026-03-06 02:03:44 +0530

    wildcard error handling

--- src/app.js
+++ src/app.js
@@ -2,29 +2,21 @@ const express = require("express");
 
 const app = express();
 
-const {adminAuth, userAuth} = require("./middlewares/auth");
-
-app.use('/admin', adminAuth)
-
-// app.use('/user', userAuth)
-
-app.post('/user/login', (req, res, next) => {
-    res.send("User Logged In")
+app.get("/getUserData", (req, res) => {
+    throw new Error("vvbjb")
+    // res.send("User data");
+    // try {
+    //     throw new Error("vvbjb")
+    // } catch (error) {
+    //     res.status(500).send("Something went wrong")
+        
+    // }
 })
-
-app.get('/user/getData', userAuth, (req, res, next) => {
-    res.send("Data Sent")
+app.use("/", (err, req, res, next) => {
+    if(err) {
+        res.status(500).send("Something went wrong")
+    }
 })
-
-app.get('/admin/getAllData', (req, res, next) => {
-    res.send("All Data Sent")
-})
-
-app.get('/admin/delete', (req, res, next) => {
-    res.send("Data Deleted")
-})
-
-
 
 app.listen(7777, () => {
   console.log("Server is running on port 7777...");
