db9601ab4939bec07fba5b8ff6b96c5034bdf756
Utkarsh Sahu <utkarshsahugithub101@gmail.com>
2026-04-02 00:27:11 +0530

    updated db password

--- src/config/database.js
+++ src/config/database.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   await mongoose.connect(
-    "mongodb+srv://namastedev:IOfpnQSuQsI1YwbY@namastenode.ongzm42.mongodb.net/devTinder",
+    "mongodb+srv://namastedev:XtjfXX21y6IUJ7W9@namastenode.ongzm42.mongodb.net/devTinder",
   );
 };
 
